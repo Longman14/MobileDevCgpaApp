a45a944cc5ca1dbe65b3f7799dc3677fae6f03d1
Longman14 <hassandamilola580@gmail.com>
2022-09-29 01:57:14 +0100

    files added

---
+++ android/app/src/main/kotlin/com/example/mobdevcgpa_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mobdevcgpa_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:mobdevcgpa_app/homePage.dart';
+
+
+void main() {
+  runApp(new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage())
+  );
+}
